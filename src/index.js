bf6c4078b5a31ed74176d5e6429ef0782f6a5ed8
Trevor Jameson <github email address>
2018-10-11 17:14:27 -0400

    ima pokemaster

--- src/index.js
+++ src/index.js
@@ -1,64 +1,114 @@
+
 document.addEventListener("DOMContentLoaded", function() {
   //pokemons is a variable imported from db.json via index.html as a script tag
   console.log(pokemons)
+
+  render(pokemons[0])
+  search()
 })
 
-//bulbasaur pokemon container
-let bulbasaur = document.createElement("div");
-bulbasaur.className = "pokemon-container";
+function render(pokeData) {
 
-//frame
-let frame = document.createElement("div");
-frame.className = "pokemon-frame";
-frame.setAttribute("style","width:230px;margin:10px;background:#fecd2f;color:#2d72fc");
+  //bulbasaur pokemon container
+  let pokemon = document.createElement("div");
+  pokemon.className = "pokemon-container";
 
-//header
-let header = document.createElement("h1");
-header.className = "center-text";
-header.innerText = "bulbasaur";
+  //frame
+  let frame = document.createElement("div");
+  frame.className = "pokemon-frame";
+  frame.setAttribute("style","width:230px;margin:10px;background:#fecd2f;color:#2d72fc");
 
-//outerbox
-let outerbox = document.createElement("div");
-outerbox.setAttribute("style","width:239px;margin:auto");
+  //header
+  let header = document.createElement("h1");
+  header.className = "center-text";
+  header.innerText = `${pokeData.name}`;
 
-//innerbox
-let innerbox = document.createElement("div");
-innerbox.setAttribute("style", "width:96px;margin:auto");
+  //outerbox
+  let outerbox = document.createElement("div");
+  outerbox.setAttribute("style","width:239px;margin:auto");
 
-//img link
-let pImg = document.createElement("img");
-pImg.setAttribute("src","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png");
+  //innerbox
+  let innerbox = document.createElement("div");
+  innerbox.setAttribute("style", "width:96px;margin:auto");
 
-
-//add img to innerbox
-innerbox.appendChild(pImg);
-
-//add innerbox to outerbox
-outerbox.appendChild(innerbox);
-
-//flip cart text
-let flip = document.createElement("p");
-flip.setAttribute("style","padding:10px;");
-flip.setAttribute("class","center-text flip-image");
-flip.dataset.pokename="bulbasaur";
-flip.dataset.action="flip-image";
-flip.innerText = "flip card";
+  //img link
+  let pImg = document.createElement("img");
+  pImg.setAttribute("src",`${pokeData.sprites.front}`);
+  // pImg.setAttribute("class", `${pokeData.name}`)
 
 
-//header is first in frame
-frame.appendChild(header);
+  //add img to innerbox
+  innerbox.appendChild(pImg);
 
-//next is outerbox
-frame.appendChild(outerbox);
+  //add innerbox to outerbox
+  outerbox.appendChild(innerbox);
 
-//last is the flip text
-frame.append(flip);
+  //flip cart text
+  let flip = document.createElement("p");
+  flip.setAttribute("style","padding:10px;");
+  flip.setAttribute("class","center-text flip-image");
+  flip.dataset.pokename=`${pokeData.name}`;
+  flip.dataset.action="flip-image";
+  flip.innerText = "flip card";
 
-//add frame to pokemon container
-bulbasaur.appendChild(frame);
+  // flip.addEventListener('click', () => {
+  //   let pic = document.querySelector(`[data-pokename=\"${pokeData.name}\"]`);
+  //   if(pic.src === pokeData.sprites.front){
+  //     pic.src === pokeData.sprites.back
+  //   }
+  // })
 
-//get the empty div inside of the id pokemon container div
-let listdiv = document.querySelector("#pokemon-container > div");
 
-//add bulbasaur to the list
-listdiv.appendChild(bulbasaur);
+  //header is first in frame
+  frame.appendChild(header);
+
+  //next is outerbox
+  frame.appendChild(outerbox);
+
+  //last is the flip text
+  frame.append(flip);
+
+  //add frame to pokemon container
+  pokemon.appendChild(frame);
+
+  //get the empty div inside of the id pokemon container div
+  let listdiv = document.querySelector("#pokemon-container > div");
+
+  // debugger
+  //add pokemon to the list
+  listdiv.appendChild(pokemon);
+}
+
+// 2. First, try to render all 150 Pokemon on the page
+  // Iterate through the pokemon json and add the html formatted pokemon box (with name and image)
+  // inside the pokemon-container div
+
+// 3. Consider implementing a 'render' function that, given a Pokemon object, returns a string representing a `div` HTML element containing the Pokemon's name and image. Play around with https://pokemon-search.netlify.com/ and inspect the DOM elements on that page to see what a div should look like.
+  // Create this function before attempting deliverable #2
+
+
+// 4. Implement a filter functionality for your Pokemon list.
+  // Create a function which takes a string param, compares the string to the name of
+  // each pokemon, and returns a nodeList of each match
+
+function filter(term) {
+  return pokemons.filter(elem => elem.name.includes(term.toLowerCase()))
+}
+
+// 5. Hint: Use the 'keyup' event listener type when listening to the input DOM element
+  // Add keyup event listener to #pokemon-search-form which takes value of #pokemon-search-input
+  // and passes it as param to filter() function
+function search() {
+  let form = document.querySelector("#pokemon-search-form")
+  form.addEventListener('keyup', () => {
+
+    console.log(filter(form.children[0].value))
+  })
+}
+
+// 6. Implement a flip functionality on each Pokemon.
+  //
+
+// Call sequence
+  // Load the page, add event listener to form
+  // On keydown, take input and search
